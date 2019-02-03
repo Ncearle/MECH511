5c3cd542195a88284c0058c691c57d8150237cd7
Nick Earle <ncearle@gmail.com>
2019-02-02 20:21:46 -0800

    A1

--- Multigrid/MECH511_A1.m
+++ Multigrid/MECH511_A1.m
@@ -3,11 +3,11 @@
 clear; clc; close all;
 
 xmin = 0; xmax = 1;
-imax = 1000;
+imax = 100;
 dx = (xmax - xmin) / imax;
 
 k = imax/2:imax;
-sig = @(w) max(abs(1 - w*(1 - cos(k*pi*dx))));
+sig = @(w) 1 - w*(1 - cos(k*pi*dx));
 
 
 sig2 = @(w1) (1 - w1*(1 - cos(k*pi*dx))) .* (1 - w1/(3*w1-1)*(1 - cos(k*pi*dx)));
@@ -20,34 +20,38 @@ x = fminsearch(g, 0.5);
 
 
 sig2 = @(w1, w2) (1 - w1*(1 - cos(k*pi*dx))) .* (1 - w2*(1 - cos(k*pi*dx)));
-
+% 
 w1 = x;
 w2 = w1./(3*w1-1);
-
-
-
-plot(k, sig2(w1, w2));
-hold on
-
-legend(sprintf('w1=%0.4f, w2=%0.4f', w1, w2));
-
-
-% y = fminsearch(sig3, w);
 % 
+% w1 = 0.6; w2 = 0.8;
+sig2 = sig2(w1, w2);
+sig = sig(2/3).^2;
+plot(k, sig2, k, sig);
+title('Amplification factor vs. wave number');
+xlabel('Wave number');
+ylabel('Amplification factor');
+% 
+legend(sprintf('\\omega_{1}=%0.4f, \\omega_{2}=%0.4f', w1, w2), '\omega = 0.6667');
+
 % x = fminsearch(sig, 0.6);
-% 
-% plot(k, sig2(y));
-% hold on
-% plot(k, sig2(0.6, 0.8, k));
-% plot(k, sig2(0.6, 1, k));
-% plot(k, sig2(0.55, 0.85, k));
 
 % figure(1);
-% for w = 1:10
+% for w = 5:10
 %     plot(k, sig(w/10));
 %     hold on;
 %     q = w/10;
-%     leg{w} = sprintf('w = %0.1f',q);
+%     leg{w-4} = sprintf('\\omega = %0.1f',q);
 % end
 % legend(leg);
+% title('Amplification factor vs. wave number');
+% xlabel('Wave number');
+% ylabel('Amplification factor');
+% 
+% sig = @(w) 1 - w*(1 - cos(k*pi*dx));
+% plot(k, sig(x));
+% legend(sprintf('\\omega = %0.4f', x));
+% title('Optimal Amplification factor vs. wave number');
+% xlabel('Wave number');
+% ylabel('Amplification factor');
 
